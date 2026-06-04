c4fe9b5e9e58f02ea46e4fcf2f47eff517cd28bc
Cyber676767 <realq5454@gmail.com>
2026-06-03 21:45:29 -0700

    Add response times and limit to monitor configuration

--- api/monitors.js
+++ api/monitors.js
@@ -16,7 +16,9 @@ export default async function handler(req, res) {
         all_time_uptime_ratio: '1',
         custom_uptime_ratios: '1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25-26-27-28-29-30-31-32-33-34-35-36-37-38-39-40-41-42-43-44-45-46-47-48-49-50-51-52-53-54-55-56-57-58-59-60-61-62-63-64-65-66-67-68-69-70-71-72-73-74-75-76-77-78-79-80-81-82-83-84-85-86-87-88-89-90',
         logs: '1',
-        logs_limit: '90'
+        logs_limit: '90',
+        response_times: '1',
+        response_times_limit: '24'
       }).toString()
     });
     const data = await response.json();
